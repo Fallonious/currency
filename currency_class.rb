6ed476d8bc2f0090d46445e8af8df18e8c74dc20
Fallonious <FallonSkinner@gmail.com>
2015-01-20 19:05:35 -0500

    1st commit

---
+++ currency_class.rb
@@ -0,0 +1,52 @@
+class Currency
+  attr_reader :amount
+
+  def initialize(amount, currency_code)
+    @amount = amount
+    @currency_code = currency_code
+  end
+
+  # def amount
+  #   return @amount
+  # end
+
+  def code
+    return @currency_code
+  end
+
+  def ==(other)
+    self.amount == other.amount && self.code == other.code
+  end
+
+  def +(other)
+    if self.code == other.code
+      return (self.amount.+(other.amount))
+    else
+      raise "currency error"
+    end
+  end
+end
+
+#
+ nicole = Currency.new(100.00, "AUD")
+ keith = Currency.new(100.00, "USD")
+#
+# cad = Currency.new(200.00 ,"CAD")
+#
+#
+# result = ( aud == aud2 )
+# puts aud
+# puts aud2
+# puts "aud is equal to aud2: #{result}"
+# #
+# # sek = Currency.new(300.00  ,"SEK")
+#
+#
+# puts aud.amount #--> 100.00
+# puts cad.code   #--> "CAD"
+# puts aud.code   #--> "AUD"
+# puts aud.amount
+# puts aud.code
+# puts cad.amount
+#
+# puts "#{aud.amount} in the currency #{aud.code}"
---
+++ currency_test.rb
@@ -0,0 +1,37 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './currency_class.rb'
+
+class CurrencyTest < Minitest::Test
+
+  def test_00_currency_class_exists
+    assert Currency
+  end
+
+  def test_01_currency_has_known_amount
+    currency = Currency.new(100.00, "AUD")
+    assert_equal 100.00, currency.amount
+  end
+
+  def test_2_currency_class_has_currency_code
+    currency = Currency.new(100.00, "AUD")
+    assert_equal "AUD", currency.code
+  end
+
+  def test_3_currency_can_be_equal_to_another_currency_object
+    nicole = Currency.new(100.00, "AUD")
+    keith = Currency.new(100.00, "AUD")
+    tom = Currency.new(100.00, "USD")
+    assert_equal true, nicole == keith
+    refute nicole == tom
+  end
+
+  def test_04_like_currency_can_be_added
+    nicole = Currency.new(100.00, "AUD")
+    keith = Currency.new(100.00, "AUD")
+    tom = Currency.new(100.00, "USD")
+    assert nicole + keith
+    refute nicole == tom
+  end
+
+end
